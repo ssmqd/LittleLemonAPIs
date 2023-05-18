f6a387a7f69c544f28027441a0fdeba9b669320c
Artem Rozycki <artemrozycki@gmail.com>
2023-05-18 18:58:21 +0200

    All requirements from the list, done!

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -131,12 +131,16 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_FILTER_BACKENDS': [
+    'DEFAULT_FILTER_CLASSES' : [
         'rest_framework.filters.OrderingFilter',
         'rest_framework.filters.SearchFilter',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 3,
+    'DEFAULT_THROTTLE_RATES': {
+		'anon':'4/minute',
+		'user':'10/minute'
+	}
 }
 
 DJOSER = {
--- littlelemon/littlelemonAPI/models.py
+++ littlelemon/littlelemonAPI/models.py
@@ -43,11 +43,11 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="order_items")
     menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     quantity = models.SmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
-    class Meta:
-        unique_together = ('order', 'menuitem')
+    # class Meta:
+    #     unique_together = ('order', 'menuitem')
