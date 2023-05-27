e7b5b178791c6c1e3cefca48432db60ea6dfa39f
ssmqd <artemrozycki@gmail.com>
2023-05-27 19:29:32 +0200

    pipenv, db changed to mysql

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littlelemon',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
